15b316acb5ca8260f187551267edc2fec8d9d3dc
jhan07n <47167831+jhan07n@users.noreply.github.com>
2019-02-09 07:28:16 -0400

    Add files via upload

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(public menuCtrl:MenuController ) { }
+
+  ngOnInit() {
+  }
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+
+}
