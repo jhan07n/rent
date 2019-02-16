4eb18fa1a9fbbe533b71c2b4ed22cc46dcaf7576
Jhan <jnavarro@codepera.com>
2019-02-16 15:47:25 -0500

    Funcionando hasta vehiculos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,10 +7,11 @@ import { version } from 'punycode';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  data:any;
+  
   id:any;
-  vehiculo = {
+  
    
+  vehiculo={
   id:'',
   descripcion:  '',
   chasis: '',
@@ -22,51 +23,53 @@ export class HomePage {
   combustible: '',
   estado: ''
   }
+  data=[];
 
   constructor(
     private storage: Storage
   ) {
-    /* this.vehiculo.id= 0; */
-   /*  this.data = {}; */
-    //Set String Value
-    this.setValue("name", "Freaky Jolly");
-    //Set Integer Value
-    this.setValue("phone", 8908904);
-    this.getid();
+  
    
-    this.getValue("vehiculos");
- 
-    /* let vehiculos =  this.vehiculo; */
-     /*  {
-        descripcion: this.vehiculo.descripcion,
-        chasis: this.vehiculo.chasis,
-        motor: this.vehiculo.motor,
-        placa: this.vehiculo.placa,
-        tipo: this.vehiculo.tipo,
-        marca: this.vehiculo.marca,
-        modelo: this.vehiculo.modelo,
-        combustible: this.vehiculo.combustible,
-        estado: this.vehiculo.estado
-      },  */
+this.id=1;
+    this.storage.forEach((value: any, key: any, iterationNumber: Number) => {
+      let k:Number;
+      this.storage.length().then((keysLength: Number) => {
+        console.log("Total Keys " + keysLength);
+        k=keysLength
+        console.log(k);
+        for(let i :any =0; i <= k; i++){
+          /* console.log("esto es for " + i + " Esto es k "+k + " Estoy es key"+ key) */
+          if (key==i){
+            this.getValue(i);
+            this.id=i+1
+            this.storage.get(i).then((val=>{
+              this.data.push(val)
+            }))
+            
+           }}})})}
     
  
-    //Set Object Value
-   /*  this.setValue("vehiculos", vehiculos); */
-
-   /*  console.log(this.data.vehiculos) */
-  }
+  
+  
  getid(){
-  this.storage.forEach((value: any, key: string, iterationNumber: Number) => {
-     if (key==="vehiculos"){
-        this.id=parseInt(value.id) + 1;
-        this.vehiculo.id = this.id;
-        console.log("el id es: " + this.vehiculo.id);
-      }
-        else {
-          this.id=1;
-            this.vehiculo.id= this.id;
-            console.log("el id els: " + this.vehiculo.id);
-        }
+   this.id=1;
+  this.storage.forEach((value: any, key: any, iterationNumber: Number) => {
+    let k:Number;
+    this.storage.length().then((keysLength: Number) => {
+      console.log("Total Keys " + keysLength);
+      k=keysLength
+      console.log(k);
+      for(let i = 0; i <= k; i++){
+        console.log("esto es for " + i + " Esto es k "+k + " Estoy es key"+ key)
+        if (key==i){
+           this.id=i + 1;
+          
+           console.log("el id es: " + i);
+         }}
+
+
+    });
+   
     
   })
  }
@@ -86,17 +89,12 @@ export class HomePage {
   }
  
   // get a key/value pair
-  getValue(key: string) {
+  getValue(key: any) {
     this.storage.get(key).then((valor => {console.table(valor)
-      this.data=valor
+      /* this.data[key]="",
+      this.data[key]=valor */
     }))
-   /*  this.storage.get(key).then((val) => {
-      console.log('get ' + key + ' ', val);
-      this.data = "";
-      this.data = val;
-    }).catch((error) => {
-      console.log('get error for ' + key + '', error);
-    }); */
+  
   }
  
   // Remove a key/value pair
@@ -123,14 +121,7 @@ export class HomePage {
       console.log("iterationNumber " + iterationNumber);
       console.log("value " + value);
      
-      /* if (key==="id"){
-        this.vehiculo.id = parseInt(value) + 1;
-        console.log("el id es: " + this.vehiculo.id);
-      }
-        else {
-            this.vehiculo.id= 1
-            console.log("el id els: " + this.vehiculo.id);
-        } */
+      
     });
   }
  
@@ -144,14 +135,19 @@ export class HomePage {
  
   // Total Keys Stored
   getKeyLength() {
+    let k:any;
     this.storage.length().then((keysLength: Number) => {
       console.log("Total Keys " + keysLength);
+      k=keysLength
     });
+    return k;
   }
  
 ver(){
-  console.table(this.vehiculo)
-  this.setValue("vehiculo"+this.id, this.vehiculo);
+  
+ this.vehiculo.id=this.id;
+this.setValue(this.id,this.vehiculo)
+this.data.push(this.vehiculo)
 }
 }
 
